dfea2b56fe7814392fc8d81e66a418c18723c26e
karunarathana <sandeepanipun7260@gmail.com>
2026-03-12 13:51:57 +0530

    Update login and register UI

--- app/(login)/sign-in/page.tsx
+++ app/(login)/sign-in/page.tsx
@@ -1,12 +1,28 @@
-'use client';
-import LoginPage from "./LoginPage";
+import LoginForm from '@/app/(components)/LoginForm';
+import Link from 'next/link'
 
-function page() {
+export default function SignIn() {
   return (
-   <div>
-      <LoginPage />
-   </div>
-  );
+    <div className=" w-full flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="w-[40%] border max-w-md border-amber-300 space-y-8 rounded-lg bg-white p-10 shadow-lg dark:bg-zinc-900">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Sign in to your account
+          </h2>
+        </div>
+        <div className="mt-8 flex justify-center items-center">
+          <LoginForm />
+        </div>
+        <p className="mt-2 text-center text-sm text-gray-600 dark:text-zinc-400">
+          Don&apos;t have an account?{" "}
+          <Link
+            href="/sign-up"
+            className="font-medium text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
 }
-
-export default page;
